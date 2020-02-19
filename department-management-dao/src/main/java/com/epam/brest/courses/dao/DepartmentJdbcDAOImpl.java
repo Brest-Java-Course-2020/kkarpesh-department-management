b43cd25441bc79fee5a8e432ae863e90bcfe26a9
kkarpesh <karpeshkir@gmail.com>
2020-02-19 13:03:55 -0500

    Add logging to dao

--- department-management-dao/src/main/java/com/epam/brest/courses/dao/DepartmentJdbcDAOImpl.java
+++ department-management-dao/src/main/java/com/epam/brest/courses/dao/DepartmentJdbcDAOImpl.java
@@ -1,15 +1,23 @@
 package com.epam.brest.courses.dao;
 
 import com.epam.brest.courses.model.Department;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 public class DepartmentJdbcDAOImpl implements DepartmentDAO {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentJdbcDAOImpl.class);
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
@@ -19,19 +27,32 @@ public class DepartmentJdbcDAOImpl implements DepartmentDAO {
 
     @Override
     public List<Department> getDepartments() {
-        List<Department> departments = namedParameterJdbcTemplate.query("SELECT d.departmentID, d.departmentName FROM department d ORDER BY d.departmentName", new DepartmentRowMapper());
+        LOGGER.trace("Get all departments");
+        String sql = "SELECT d.departmentID, d.departmentName FROM department d ORDER BY d.departmentName";
+        List<Department> departments = namedParameterJdbcTemplate.query(sql, new DepartmentRowMapper());
         return departments;
     }
 
     @Override
     public Department getDepartmentById(Integer departmentId) {
-        Department department = namedParameterJdbcTemplate.queryForObject("SELECT d.departmentID, d.departmentName FROM department d WHERE departmentId = :departmentId", new MapSqlParameterSource("departmentId", departmentId), new DepartmentRowMapper());
+        String sql = "SELECT d.departmentID, d.departmentName FROM department d WHERE departmentId = :departmentId";
+        Department department = namedParameterJdbcTemplate.queryForObject(sql, new MapSqlParameterSource("departmentId", departmentId), new DepartmentRowMapper());
         return department;
     }
 
     @Override
     public Department addDepartment(Department department) {
-        return null;
+        String sql = "INSERT INTO department(departmentName) VALUES (:departmentName)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("departmentName", department.getDepartmentName());
+
+        namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource(paramMap), keyHolder);
+
+        Integer newDepartmentId = keyHolder.getKey().intValue();
+        department.setDepartmentId(newDepartmentId);
+        return department;
     }
 
     @Override
