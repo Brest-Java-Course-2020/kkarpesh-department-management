90e2abe1730d4bbd00b14b260aa7c8016e4e620a
kkarpesh <karpeshkir@gmail.com>
2020-02-23 16:00:01 -0500

    add methods

--- department-management-dao/src/main/java/com/epam/brest/courses/dao/DepartmentJdbcDAOImpl.java
+++ department-management-dao/src/main/java/com/epam/brest/courses/dao/DepartmentJdbcDAOImpl.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
@@ -21,6 +22,12 @@ public class DepartmentJdbcDAOImpl implements DepartmentDAO {
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
+    private final String SELECT_ALL_DEPARTMENTS = "SELECT d.departmentID, d.departmentName FROM department d ORDER BY d.departmentName";
+    private final String SELECT_DEPARTMENT = "SELECT d.departmentID, d.departmentName FROM department d WHERE departmentId = :departmentId";
+    private final String ADD_DEPARTMENT = "INSERT INTO department(departmentName) VALUES (:departmentName)";
+    private final String UPDATE_DEPARTMENT = "UPDATE DEPARTMENT SET departmentName= :=departmentName WHERE departmentId = :departmentId";
+
+
     public DepartmentJdbcDAOImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
@@ -28,41 +35,46 @@ public class DepartmentJdbcDAOImpl implements DepartmentDAO {
     @Override
     public List<Department> getDepartments() {
         LOGGER.debug("Get all departments");
-        String sql = "SELECT d.departmentID, d.departmentName FROM department d ORDER BY d.departmentName";
-        List<Department> departments = namedParameterJdbcTemplate.query(sql, new DepartmentRowMapper());
+        List<Department> departments = namedParameterJdbcTemplate.query(SELECT_ALL_DEPARTMENTS, new DepartmentRowMapper());
         return departments;
     }
 
     @Override
     public Department getDepartmentById(Integer departmentId) {
         LOGGER.debug("Get departments by ID {}", departmentId);
-        String sql = "SELECT d.departmentID, d.departmentName FROM department d WHERE departmentId = :departmentId";
-        Department department = namedParameterJdbcTemplate.queryForObject(sql, new MapSqlParameterSource("departmentId", departmentId), new DepartmentRowMapper());
+        Department department = namedParameterJdbcTemplate.queryForObject(SELECT_DEPARTMENT, new MapSqlParameterSource("departmentId", departmentId), new DepartmentRowMapper());
         return department;
     }
 
     @Override
-    public Department addDepartment(Department department) {
-        String sql = "INSERT INTO department(departmentName) VALUES (:departmentName)";
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-
-        Map<String, Object> paramMap = new HashMap<>();
+    public int addDepartment(Department department) {
+        Map<String, Object> paramMap = new HashMap<String, Object>();
         paramMap.put("departmentName", department.getDepartmentName());
-
-        namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource(paramMap), keyHolder);
-
-        Integer newDepartmentId = keyHolder.getKey().intValue();
-        department.setDepartmentId(newDepartmentId);
-        return department;
+        return namedParameterJdbcTemplate.update(ADD_DEPARTMENT, paramMap);
     }
 
     @Override
     public void updateDepartment(Department department) {
-
+        SqlParameterSource namedPArameters = new MapSqlParameterSource()
+                .addValue("departmentName", department.getDepartmentName())
+                .addValue("departmentId",department.getDepartmentId());
+        int status = namedParameterJdbcTemplate.update(UPDATE_DEPARTMENT, namedPArameters);
+        if (status!=0){
+            LOGGER.debug("Department data updated for ID {}", department.getDepartmentId());
+        } else{
+            LOGGER.debug("No department found with ID {}", department.getDepartmentId());
+                    }
     }
 
     @Override
     public void deleteDepartment(Integer departmentId) {
+        SqlParameterSource namedParameters = new MapSqlParameterSource("departmentId", departmentId);
+        int status = namedParameterJdbcTemplate.update(DELETE_DEPARTMENT, namedParameters);
+        if (status!=0){
+            LOGGER.debug("Department data deleted for ID {}", departmentId);
+        } else{
+            LOGGER.debug("No department found with ID {}", departmentId);
+        }
 
     }
 
